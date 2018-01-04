d1ce63b17599707583f19d5beb33b000a4927813
s-andrew <anso2393@yandex.ru>
2018-01-04 14:13:40 +0300

    Create project

---
+++ src/main/java/com/kek/kekMovie/KekMovieApplication.java
@@ -0,0 +1,12 @@
+package com.kek.kekMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KekMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KekMovieApplication.class, args);
+	}
+}
