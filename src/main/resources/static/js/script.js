a518bf63f416b91af200dff16d5905339b55c22f
s-andrew <anso2393@yandex.ru>
2018-01-11 18:00:43 +0300

    Update web client

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -34,14 +34,17 @@ function addResultRow(parentElement, cathegory, name, id){
                     resultDiv.appendChild(ul);
                 }
                 if (cathegory == "movies_by_person")
-                    globalSearchResultProcess(data);                
+                    personResultProcess(data);
             });
     });
     li.appendChild(button);
     parentElement.appendChild(li);
 }
 
-searchBtn.addEventListener("click", function(){
+searchBtn.addEventListener("click", search);
+
+
+function search(){
     resultDiv.innerHTML = "";
     info.style.display = "none";
     fetch("/movie_api/global_search?f="+searchField.value, {credentials: "include", method: "get"})
@@ -51,8 +54,7 @@ searchBtn.addEventListener("click", function(){
             console.log("Error:")
             console.log(url);
         });
-});
-
+}
 
 function globalSearchResultProcess(data){
     DATA = data;
@@ -66,13 +68,58 @@ function globalSearchResultProcess(data){
     }
 }
 
+function personResultProcess(data){
+    DATA = data;
+    for(var categoryKey in data){
+        let h2 = document.createElement("h2");
+        h2.innerText = categoryKey.replace("movies_by_", "");
+        resultDiv.appendChild(h2);
+        let ul = document.createElement("ul");
+        data[categoryKey].forEach(element => addResultRow(ul, "movies", element["value"], element["key"]));
+        resultDiv.appendChild(ul);
+    }
+}
+
 function movieResultProcess(data){
     DATA = data;
-    let h2 = document.createElement("h2");
-    h2.innerText = data["title"];
-    let p = document.createElement("p");
-    p.innerText = JSON.stringify(data, null, 4);
-    resultDiv.appendChild(h2);
-    resultDiv.appendChild(p);
+
+    resultDiv.innerHTML =
+    "<h2>" + data["title"] + "</h2>" +
+    "<p>" + data["overview"] + "</p>" +
+    "<table>" +
+    "   <tbody>" +
+    "       <tr>" +
+    "           <td>Runtime</td>" +
+    "           <td>" + data["runtime"] + "</td>" +
+    "       </tr>" +
+    "       <tr>" +
+    "           <td>Budget</td>" +
+    "           <td>" + data["budget"] + "</td>" +
+    "       </tr>" +
+    "       <tr>" +
+    "            <td>Revenue</td>" +
+    "            <td>" + data["revenue"] + "</td>" +
+    "       </tr>" +
+    "       <tr>" +
+    "           <td>Original language</td>" +
+    "           <td>" + data["originalLanguage"] + "</td>" +
+    "       </tr>" +
+    "       <tr>" +
+    "           <td>Vote Average</td>" +
+    "           <td>" + data["voteAverage"] + "</td>" +
+    "       </tr>" +
+    "   </tbody>" +
+    "</table>";
+
 }
+
+
+
+document.getElementById("searchForm").addEventListener("keyup", function(e) {
+    console.log(e)
+    if (e.keyCode === 13){
+        e.preventDefault();
+        search();
+    }
+})
 
